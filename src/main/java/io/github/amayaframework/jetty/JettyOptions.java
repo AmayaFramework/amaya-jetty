03f2d537386b4ef362123e5c76271a6652cf81bb
Roman Bakaldin <gbakaldin@gmail.com>
2025-06-21 19:10:18 +0300

    Update jetty options with new features

--- src/main/java/io/github/amayaframework/jetty/JettyOptions.java
+++ src/main/java/io/github/amayaframework/jetty/JettyOptions.java
@@ -1,39 +1,68 @@
 package io.github.amayaframework.jetty;
 
+import com.github.romanqed.jtype.JType;
+import io.github.amayaframework.http.HttpVersion;
+import io.github.amayaframework.jetty.old.JettyServerFactory;
+import io.github.amayaframework.options.Key;
+
+import java.net.InetSocketAddress;
+import java.util.Map;
+
 /**
- * A class containing the names of the main server options supported by the {@link JettyServerFactory}.
+ * A class containing the keys for the server options supported by the {@link JettyServerFactory}.
  */
 public final class JettyOptions {
+
     /**
-     * The name of the server options group.
-     *
-     * @see io.github.amayaframework.options.GroupOptionSet
+     * The key for the listened port option.
+     * <br>
+     * Required type: {@link Integer}.
      */
-    public static final String GROUP = "jetty";
+    public static final Key<Integer> PORT = Key.of("port", Integer.class);
     /**
-     * The name of the listen port option. Required type: {@link Integer}.
+     * The key for the listened ports option.
+     * <br>
+     * Required type: {@link Iterable} of {@link Integer}.
      */
-    public static final String PORT = "port";
+    public static final Key<Iterable<Integer>> PORTS = Key.of("ports", new JType<>(){});
     /**
-     * The name of the listen ports option. Required type: {@link Iterable} of {@link Integer}.
+     * The key for the listened ip address option.
+     * <br>
+     * Required type: {@link java.net.InetSocketAddress}.
      */
-    public static final String PORTS = "ports";
+    public static final Key<InetSocketAddress> IP = Key.of("ip", InetSocketAddress.class);
     /**
-     * The name of the listen ip option. Required type: {@link java.net.InetSocketAddress}.
+     * The key for the listened ip addresses option.
+     * <br>
+     * Required type: {@link Iterable} of {@link java.net.InetSocketAddress}.
      */
-    public static final String IP = "ip";
+    public static final Key<Iterable<InetSocketAddress>> IPS = Key.of("ips", new JType<>(){});
     /**
-     * The name of the listen ips option. Required type: {@link Iterable} of {@link java.net.InetSocketAddress}.
+     * The key for the http version option.
+     * <br>
+     * Required type: {@link io.github.amayaframework.http.HttpVersion}.
      */
-    public static final String IPS = "ips";
+    public static final Key<HttpVersion> HTTP_VERSION = Key.of("http_version", HttpVersion.class);
     /**
-     * The name of the initial http version. Required type: {@link io.github.amayaframework.http.HttpVersion}.
-     */
-    public static final String HTTP_VERSION = "http_version";
-    /**
-     * The name of the flag determines whether the server will support http sessions. Required type: {@link Boolean}.
+     * The key for the flag determines whether the server will support http sessions.
+     * <br>
+     * Required type: {@link Boolean}.
      */
     public static final String ENABLE_SESSIONS = "enable_sessions";
+
+    /**
+     * The key for the common ssl keystore option.
+     * <br>
+     * Required type: {@link SSLKeystore}.
+     */
+    public static final Key<SSLKeystore> KEYSTORE = Key.of("keystore", SSLKeystore.class);
+
+    /**
+     * The key for the ssl keystore mapping option.
+     * <br>
+     * Required type: {@link Map} of {@link InetSocketAddress} -&gt; {@link SSLKeystore}.
+     */
+    public static final Key<Map<InetSocketAddress, SSLKeystore>> KEYSTORES = Key.of("keystores", new JType<>(){});
 
     private JettyOptions() {
     }
